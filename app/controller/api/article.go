2d50aeed27c2b4310e35aade6bc79fb9e837f1da
wurui <wuruiwm@qq.com>
2020-10-12 17:30:20 +0800

    gorm实战 暂存

--- app/controller/api/article.go
+++ app/controller/api/article.go
@@ -1,11 +1,16 @@
 package api
 
 import (
+	"fmt"
+	"gin_dev/app/model"
 	"gin_dev/app/response"
 	"github.com/gin-gonic/gin"
 )
 
 func GetArticleList(c *gin.Context){
+	var article model.Article
+	model.Db.First(&article)
+	fmt.Println(article)
 	response.Success(c,"获取成功",nil)
 	return
 }
